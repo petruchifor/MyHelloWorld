1a5c441700f5177ff7aa7e001df4ea8a1d7a374a
Petru Chifor <petru.chifor@telus.com>
2021-08-06 11:21:50 -0400

    tyest

---
+++ src/main/java/com/vog/tst/HelloWorldSBApp.java
@@ -0,0 +1,13 @@
+package com.vog.tst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldSBApp {
+	
+	public static void main (String[] args) {
+		SpringApplication.run(HelloWorldSBApp.class, args);
+	}
+
+}
---
+++ src/main/java/com/vog/tst/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.vog.tst.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	@RequestMapping(value="/")
+	public String hello() {
+		return "Hello Cloudly World !";
+	}
+	
+}
